e4297d2a07f0149461aa098667a0ee71f61cc875
r4coon77 <139823112+r4coon77@users.noreply.github.com>
2023-10-25 17:58:43 +0100

    save

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    return str.split('').reverse().join('')
 };
 
+reverseString("");
+8
 // Do not edit below this line
 module.exports = reverseString;
